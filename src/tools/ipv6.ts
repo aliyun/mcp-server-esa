b0dbce7214bb6e9d21306c9406cbf3d75745375f
chaunMa <chaunma@163.com>
2025-06-16 19:55:52 +0800

    modified:   src/tools/ipv6.ts 	modified:   src/tools/managedTransform.ts

--- src/tools/ipv6.ts
+++ src/tools/ipv6.ts
@@ -1,31 +1,31 @@
-import { UpdateIPv6Request, GetIPv6Request } from "@alicloud/esa20240910";
-import api from "../utils/service.js";
-import { CallToolRequest, Tool } from "@modelcontextprotocol/sdk/types.js";
+import { UpdateIPv6Request, GetIPv6Request } from '@alicloud/esa20240910';
+import api from '../utils/service.js';
+import { CallToolRequest, Tool } from '@modelcontextprotocol/sdk/types.js';
 
 export const UPDATE_IPV6_TOOL: Tool = {
-  name: "update_ipv6",
-  description: "Modifies the IPv6 configuration of a website.",
+  name: 'update_ipv6',
+  description: 'Modifies the IPv6 configuration of a website.',
   inputSchema: {
-    type: "object",
+    type: 'object',
     properties: {
       siteId: {
-        type: "number",
-        description: "The website ID, which can be obtained by calling the ListSites operation.",
+        type: 'number',
+        description: 'The website ID, which can be obtained by calling the ListSites operation.',
         example: [12228828888],
       },
       enable: {
-        type: "string",
-        description: "Specifies whether to enable IPv6.",
-        enum: ["on", "off"],
+        type: 'string',
+        description: 'Specifies whether to enable IPv6.',
+        enum: ['on', 'off'],
       },
       region: {
-        type: "string",
+        type: 'string',
         description:
-          "Enable IPV6 in the region.",
-        enum: ["x.x", "cn.cn"],
+          'Enable IPV6 in the region.',
+        enum: ['x.x', 'cn.cn'],
       },
     },
-    required: ["siteId", "enable"],
+    required: ['siteId', 'enable'],
     annotations: {
       readOnlyHint: false,
       destructiveHint: false,
@@ -35,18 +35,18 @@ export const UPDATE_IPV6_TOOL: Tool = {
 };
 
 export const GET_IPV6_TOOL: Tool = {
-  name: "get_ipv6",
-  description: "Queries the IPv6 configuration of a website.",
+  name: 'get_ipv6',
+  description: 'Queries the IPv6 configuration of a website.',
   inputSchema: {
-    type: "object",
+    type: 'object',
     properties: {
       siteId: {
-        type: "number",
-        description: "The website ID, which can be obtained by calling the ListSites operation.",
+        type: 'number',
+        description: 'The website ID, which can be obtained by calling the ListSites operation.',
         example: [12228828888],
       },
     },
-    required: ["siteId"],
+    required: ['siteId'],
     annotations: {
       readOnlyHint: true,
       destructiveHint: false,
@@ -61,7 +61,7 @@ export const update_ipv6 = async (request: CallToolRequest) => {
   );
 
   return {
-    content: [{ type: "text", text: JSON.stringify(res) }],
+    content: [{ type: 'text', text: JSON.stringify(res) }],
     success: true,
   };
 };
@@ -70,7 +70,7 @@ export const get_ipv6 = async (request: CallToolRequest) => {
   const res = await api.getIPv6(request.params.arguments as GetIPv6Request);
 
   return {
-    content: [{ type: "text", text: JSON.stringify(res) }],
+    content: [{ type: 'text', text: JSON.stringify(res) }],
     success: true,
   };
 };
--- src/tools/managedTransform.ts
+++ src/tools/managedTransform.ts
@@ -1,46 +1,46 @@
 import {
   UpdateManagedTransformRequest,
   GetManagedTransformRequest,
-} from "@alicloud/esa20240910";
-import api from "../utils/service.js";
-import { CallToolRequest, Tool } from "@modelcontextprotocol/sdk/types.js";
+} from '@alicloud/esa20240910';
+import api from '../utils/service.js';
+import { CallToolRequest, Tool } from '@modelcontextprotocol/sdk/types.js';
 
 export const UPDATE_MANAGED_TRANSFORM_TOOL: Tool = {
-  name: "update_managed_transform",
-  description: "Modifies the configuration of managed transforms for your website.",
+  name: 'update_managed_transform',
+  description: 'Modifies the configuration of managed transforms for your website.',
   inputSchema: {
-    type: "object",
+    type: 'object',
     properties: {
       siteId: {
-        type: "number",
-        description: "The website ID, which can be obtained by calling the ListSites operation.",
+        type: 'number',
+        description: 'The website ID, which can be obtained by calling the ListSites operation.',
         example: [12228828888],
       },
       addClientGeolocationHeader: {
-        type: "string",
+        type: 'string',
         description:
-          "Specifies whether to include the header that indicates the geographical location of a client in an origin request.",
-        enum: ["on","off"],
+          'Specifies whether to include the header that indicates the geographical location of a client in an origin request.',
+        enum: ['on','off'],
       },
       siteVersion: {
-        type: "number",
+        type: 'number',
         description:
-          "The version number of the website. You can use this parameter to specify a version of your website to apply the feature settings. By default, version 0 is used.",
+          'The version number of the website. You can use this parameter to specify a version of your website to apply the feature settings. By default, version 0 is used.',
         example: [0],
       },
       addRealClientIpHeader: {
-        type: "string",
+        type: 'string',
         description:
-          'Specifies whether to include the "ali-real-client-ip" header that indicates the \'s real IP address in an origin request.',
-        enum: ["on","off"],
+          'Specifies whether to include the \'ali-real-client-ip\' header that indicates the \'s real IP address in an origin request.',
+        enum: ['on','off'],
       },
       realClientIpHeaderName: {
-        type: "string",
-        description: "The actual client IP header name.",
-        example: ["test_header"],
+        type: 'string',
+        description: 'The actual client IP header name.',
+        example: ['test_header'],
       },
     },
-    required: ["siteId"],
+    required: ['siteId'],
     annotations: {
       readOnlyHint: false,
       destructiveHint: false,
@@ -50,24 +50,24 @@ export const UPDATE_MANAGED_TRANSFORM_TOOL: Tool = {
 };
 
 export const GET_MANAGED_TRANSFORM_TOOL: Tool = {
-  name: "get_managed_transform",
-  description: "Query Managed Transform Configuration.",
+  name: 'get_managed_transform',
+  description: 'Query Managed Transform Configuration.',
   inputSchema: {
-    type: "object",
+    type: 'object',
     properties: {
       siteId: {
-        type: "number",
-        description: "Site ID, which can be obtained by calling ListSites.",
+        type: 'number',
+        description: 'Site ID, which can be obtained by calling ListSites.',
         example: [12228828888],
       },
       siteVersion: {
-        type: "number",
+        type: 'number',
         description:
-          "The version number of the site. For sites with version management enabled, you can use this parameter to specify the effective version of the configuration, defaulting to version 0.",
+          'The version number of the site. For sites with version management enabled, you can use this parameter to specify the effective version of the configuration, defaulting to version 0.',
         example: [0],
       },
     },
-    required: ["siteId"],
+    required: ['siteId'],
     annotations: {
       readOnlyHint: true,
       destructiveHint: false,
@@ -82,7 +82,7 @@ export const update_managed_transform = async (request: CallToolRequest) => {
   );
 
   return {
-    content: [{ type: "text", text: JSON.stringify(res) }],
+    content: [{ type: 'text', text: JSON.stringify(res) }],
     success: true,
   };
 };
@@ -93,7 +93,7 @@ export const get_managed_transform = async (request: CallToolRequest) => {
   );
 
   return {
-    content: [{ type: "text", text: JSON.stringify(res) }],
+    content: [{ type: 'text', text: JSON.stringify(res) }],
     success: true,
   };
 };
